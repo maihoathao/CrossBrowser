cb01a310aef7159642583e52789c68d1f6ec0dea
maihoathao <maihoathaoit@gmail.com>
2019-06-24 16:46:39 +0700

    first test

---
+++ src/main/java/base/SetupTestDriver.java
@@ -0,0 +1,29 @@
+package base;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SetupTestDriver {
+    private static String nodeUrl = "http://localhost:4444/wd/hub";
+
+    public static DesiredCapabilities SetupBrowser(String browserType){
+        if ("chrome".equals(browserType)) {
+            System.out.println("Opening Chrome");
+            return DesiredCapabilities.chrome();
+        } else if ("firefox".equals(browserType)) {
+            System.out.println("Opening firefox");
+            return DesiredCapabilities.firefox();
+        } else if ("IE".equals(browserType)) {
+            System.out.println("Opening IE");
+            return DesiredCapabilities.internetExplorer();
+        }
+        System.out.println("Browser:" + browserType + "is invalid, Launching default browser is Firefox");
+        return DesiredCapabilities.firefox();
+    }
+    public static RemoteWebDriver getDriver(String browser) throws MalformedURLException {
+        return new RemoteWebDriver(new URL(nodeUrl),SetupBrowser(browser));
+    }
+}
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,41 +1,59 @@
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.remote.DesiredCapabilities;
+/*
+@author Hoi
+Date Jun 17 19
+ */
+import base.SetUp;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 
 public class MyTest {
-    RemoteWebDriver chromeDriver, firefoxDriver;
-    String baseUrl, nodeUrl;
+    RemoteWebDriver chromeTest, firefoxTest;
+    String urlTest;
+    WebDriver driver;
 
-    @BeforeTest
-    public void setUp() throws MalformedURLException{
-        baseUrl = "https://www.google.com";
-        nodeUrl = "http://localhost:4444/wd/hub";
-        Capabilities chromeCap = DesiredCapabilities.chrome();
-        Capabilities firefoxCap = DesiredCapabilities.firefox();
-        chromeDriver = new RemoteWebDriver(new URL(nodeUrl),chromeCap);
-        firefoxDriver = new RemoteWebDriver(new URL(nodeUrl),firefoxCap);
-    }
+    public static final String email = "maihoathao2004@yahoo.com";
+    public static final String pass = "Xanh$&@Ngoc654";
 
-    @AfterTest
-    public void afterTest(){
-        chromeDriver.quit();
-        firefoxDriver.quit();
-    }
-     @Test
-     public void myTest(){
-         // run against chrome
-        chromeDriver.get(baseUrl);
-        System.out.println("Chrome search:" + chromeDriver.getTitle());
+    @BeforeClass(alwaysRun = true)
+//    @Parameters({"node","url"})
+    @Test
+     public void myTest() throws MalformedURLException{
+
+        SetUp setUp = new SetUp();
+        urlTest = setUp.setUrl();
+
+        // run against chrome
+        chromeTest = setUp.setChromeDriver();
+        chromeTest.get(urlTest);
+        System.out.println("Chrome: " + chromeTest.getTitle());
+//        Assert.assertTrue(chromeTest.getTitle().contentEquals("Google"));
 
          // run against firefox
-         firefoxDriver.get(baseUrl);
-         System.out.println("Firefox search:" + firefoxDriver.getTitle());
+        firefoxTest = setUp.setFirefoxDriver();
+        firefoxTest.get(urlTest);
+        System.out.println("Firefox: " + firefoxTest.getTitle());
+//        Assert.assertTrue(firefoxTest.getTitle().contentEquals("Google"));
      }
+
+     @Test
+     public void loginFb() throws MalformedURLException{
+         driver.findElement(By.id("id")).sendKeys(email);
+         driver.findElement(By.id("pass")).sendKeys(pass);
+         driver.findElement(By.id("loginbutton")).click();
+     }
+
+     @AfterTest
+     public void afterTest(){
+        chromeTest.quit();
+        firefoxTest.quit();
+    }
 }
